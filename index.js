efc5469a16366ed905282599331a002cd16e0107
Will Fairclough <will.fairclough@suitespottechnology.com>
2019-11-27 10:54:27 -0500

    Added api for base64 options

--- index.js
+++ index.js
@@ -33,13 +33,37 @@ function hashInt(x) {
 }
 
 app.get('/_health', (req, res) => {
-  console.log('/_health');
+  console.log(req.path);
   res.send('ok');
 });
 
+app.get('/avatar/:base64Options.png', (req, res) => {
+  console.log(req.path);
+  const encodedParams = req.params['base64Options'];
+  const decodedParams = encodedParams ? Buffer.from(encodedParams, 'base64').toString('utf-8') : null;
+  const options = decodedParams ? JSON.parse(decodedParams) : (req.query || {});
+  const [reg, dark, light, medium] = generatorColors(options);
+  const colorTypes = { reg, dark, light, medium };
+  const option = {
+    width: 200,
+    text: 'WF',
+    color: '#' + colorTypes[req.colorType || 'dark'],
+    fontColor: '#' + light,
+    shape: 'square', //'circle',
+    ...options,
+  };
+
+  const avatarGenerator = new AvatarGenerator();
+  avatarGenerator.generate(option, image => {
+    image
+      .stream('png')
+      .pipe(res.contentType('image/png').status(200));
+  });
+});
+
 app.get('/avatar/generate', (req, res) => {
-  console.log('/avatar/generate');
-  const [reg, dark, light, medium] = generatorColorsFromRequest(req);
+  console.log(req.path);
+  const [reg, dark, light, medium] = generatorColors(req.query);
   const colorTypes = { reg, dark, light, medium };
   const option = {
     width: 200,
@@ -58,11 +82,10 @@ app.get('/avatar/generate', (req, res) => {
   });
 });
 
-function generatorColorsFromRequest(req = { params: {}, query: {} }) {
+function generatorColors(options, schemeType = 'mono') {
   const scheme = new ColorScheme();
-  const schemeType = req.params['scheme'] || 'mono';
-  const [variation] = Array.isArray(req.query.variation) ? req.query.variation : [req.query.variation || 'hard'];
-  const [hue] = Array.isArray(req.query.hue) ? req.query.hue : [req.query.hue];
+  const [variation] = Array.isArray(options.variation) ? options.variation : [options.variation || 'hard'];
+  const [hue] = Array.isArray(options.hue) ? options.hue : [options.hue];
   let hueDegrees = hue ? parseInt(hue, 10) : 21;
   if (hue === 'random') {
     hueDegrees = Math.random() * 360;
@@ -77,8 +100,8 @@ function generatorColorsFromRequest(req = { params: {}, query: {} }) {
 }
 
 app.get('/colors/scheme/:scheme.html', (req, res) => {
-  console.log('/colors/scheme/:scheme.html');
-  const [reg, dark, light, med] = generatorColorsFromRequest(req);
+  console.log(req.path);
+  const [reg, dark, light, med] = generatorColors(req.query, req.params['scheme']);
   res.status(200)
     .contentType('text/html')
     .send(`
@@ -90,8 +113,8 @@ app.get('/colors/scheme/:scheme.html', (req, res) => {
 });
 
 app.get('/colors/scheme/:scheme', (req, res) => {
-  console.log('/colors/scheme/:scheme');
-  const colors = generatorColorsFromRequest(req);
+  console.log(req.path);
+  const colors = generatorColors(req, req.params['scheme']);
 
   res.status(200)
     .contentType('application/json')
